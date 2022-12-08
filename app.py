2b0d21aeec901c01f4f16566de82f7f8e1a2e9d1
Chao Zhou <Czhou0k@gmail.com>
2022-12-08 17:35:36 -0500

    installed sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  complete = db.Column(db.Boolean)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+  name = request.form.get('name')
+  new_task = Todo(name=name, complete=False)
+  db.session.add(new_task)
+  db.session.commit()
+  return redirect(url_for('home'))
+
+if __name__ == "__main__":
+
+  app.run(debug=True)
