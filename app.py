0449e82f5bc5fdb531e3153749b9d4dcc53fa150
Chao Zhou <Czhou0k@gmail.com>
2022-12-11 10:58:24 -0500

    added a jwt token

--- app.py
+++ app.py
@@ -50,7 +50,10 @@ def riderLogin():
   if not data['username'] or not data['password']:
     return "Please enter the required fields", 400
   rider_login = Riders.query.filter_by(username=data['username']).first()
-
+  print(rider_login)
+  hashed_password = rider_login.password
+  encoded_jwt= jwt.encode({'username':data['username']},SECRET_KEY, algorithm="HS256" )
+  return encoded_jwt
 
 
 
