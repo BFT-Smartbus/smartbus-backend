a33c7bb6c05d017580cb27d58ac055e869a56a22
tonyxuan2021 <tonyyexuan@gmail.com>
2022-12-14 16:46:32 -0800

    resolve pr comment by scott and Aaron

--- modals/Riders.py
+++ modals/Riders.py
@@ -4,4 +4,3 @@ class Riders(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(100))
   password = db.Column(db.String(100))
-  something = db.Column(db.String(100))
