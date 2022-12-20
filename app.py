505b8601f7c9f1024ce19971956293c959bfacf0
tonyxuan2021 <tonyyexuan@gmail.com>
2022-12-20 08:10:31 -0800

    created PR beacause of formatted with black formatter

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ with app.app_context():
 
 if __name__ == "__main__":
 
-  app.run(debug=True)
+    app.run(debug=True)
--- modals/Riders.py
+++ modals/Riders.py
@@ -1,6 +1,7 @@
 from config import db
 
+
 class Riders(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(100))
-  password = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
