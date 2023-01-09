cf816fdb49a0e1f8ce3f02f05b3f03e47f379870
tonyxuan2021 <tonyyexuan@gmail.com>
2023-01-09 11:53:58 -0800

    code clean up

--- models/Routes.py
+++ models/Routes.py
@@ -1,16 +1,7 @@
 from config import db
-import enum
-from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
 from dataclasses import dataclass
 
 STRING_SIZE = 2048
-
-
-# class Status(enum.Enum):
-#     WAITING = "waiting"
-#     PICKED_UP = "picked_up"
-#     DROPPED_OFF = "dropped_off"
 
 
 @dataclass
@@ -18,6 +9,5 @@ class Routes(db.Model):
     routeId: str = db.Column(db.String, primary_key=True)
     timestamp: int = db.Column(db.Integer, nullable=False)
     duration: int = db.Column(db.Integer, nullable=False)
-    # status: Status = db.Column(sqlalchemy.types.Enum(Status))
     status: str = db.Column(db.String, nullable=False)
     polyline: str = db.Column(db.String(STRING_SIZE), nullable=False)
