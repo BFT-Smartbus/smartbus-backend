ec09dcfa01deb189c86b1cce6610996f19dd5473
Chao Zhou <Czhou0k@gmail.com>
2023-01-11 18:19:10 -0500

    change sms to a function

--- send_sms.py
+++ send_sms.py
@@ -4,14 +4,15 @@ load_dotenv()
 import os
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
-message = client.messages \
+def SMS():
+  account_sid = os.environ['TWILIO_ACCOUNT_SID']
+  auth_token = os.environ['TWILIO_AUTH_TOKEN']
+  client = Client(account_sid, auth_token)
+  message = client.messages \
                 .create(
-                     body="ANIME IS AWESOME!",
+                     body="ANIME IS AWESOME IT IS OVER 9000!",
                      from_= os.environ['FROM_NUMBER'],
                      to= os.environ['TO_NUMBER']
                  )
 
+SMS()
